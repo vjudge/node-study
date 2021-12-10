3c8ab6f03812d0ad62aa177375351d69f2d069e3
vjudge <gradonday@gmail.com>
2021-12-10 11:08:54 +0800

    init consul client

--- extension-libs/consul/consul-client.js
+++ extension-libs/consul/consul-client.js
@@ -1,7 +1,44 @@
+const config = require('config')
 const Consul = require('consul')
 
-const consul = new Consul({
-    host: '127.0.0.1',
-    port: 8500,
-    promisify: true
-})
+class ConsulClient {
+    constructor() {
+        this.consul = new Consul({
+            host: config.get('consul.host'),
+            port: config.get('consul.port'),
+            promisify: true
+        })
+        // this.consul = new Consul({
+        //     baseUrl: config.get('consul.url') + '/v1',
+        //     defaults: {
+        //         token: config.get('consul.token')
+        //     },
+        //     promisify: true
+        // })
+    }
+
+    async register (cfg) {
+        this.consul.agent.service.register(cfg, function(err, result){
+            if(err){
+                throw err;
+            }
+            console.log(cfg.name +' 注册成功！');
+        })
+    }
+
+    async getKV (key) {
+        const result = await this.consul.kv.get(key);
+        if (!result) {
+            return null;
+        }
+        return JSON.parse(result.Value);
+    }
+
+    async setKV (key, val) {
+        this.consul.kv.set(key, JSON.stringify(val))
+        return true
+    }
+
+}
+
+module.exports = new ConsulClient()
