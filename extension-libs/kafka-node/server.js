e25edc3ddba2a3a21af86be613530af49eb7cfd4
vjudge <gradonday@gmail.com>
2022-01-07 19:09:06 +0800

    update server

--- extension-libs/kafka-node/server.js
+++ extension-libs/kafka-node/server.js
@@ -1,20 +1,43 @@
 const sdk = require('./sdk')
 const kafkaClient = require('./kafka-client')
+const Kafka = require("kafka-node");
 
 main();
 
 async function main() {
-    await producerMsg()
+    // await produceMsg()
+    await consumeMsg()
+    // await consumeMsgByGrp()
+    // await createTopic()
 }
 
-async function producerMsg () {
-    let result = await kafkaClient.producerMsg('kafka-node-test')
-    console.log('result:', result)
+async function produceMsg () {
+    let topic = 'kafka-node-test';
+    const kv = new Kafka.KeyedMessage('key', 'message')
+    const payload = [
+        { topic, message: 'hi', partition: 0 },
+        { topic, message: ['Hi', 'vjudge', kv], partition: 1 }
+    ]
+    let result = await kafkaClient.produceMsg(payload)
+    console.log('producerMsg result:', result)
     return result
 }
 
+async function consumeMsg () {
+    let topic = 'kafka-node-test';
+    await kafkaClient.consumeMsg(topic, 1)
+}
+
+async function consumeMsgByGrp () {
+    let topic = 'kafka-node-test';
+    await kafkaClient.consumeMsgByGrp(topic)
+}
+
 async function createTopic () {
+    let topic = 'kafka-node-test';
+    await kafkaClient.createTopic(topic);
+
     let thisIP = sdk.getIPAddress();
-    let topic = await kafkaClient.createTopic(thisIP);
-    console.log('topic:', topic)
+    let result = await kafkaClient.createTopic('serv-kn-' + thisIP);
+    console.log('createTopic result:', result)
 }
