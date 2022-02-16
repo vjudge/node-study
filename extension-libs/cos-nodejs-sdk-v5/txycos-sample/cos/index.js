1074ca11518d1f4d1570bf948d689cedb7bb13ef
vjudge <gradonday@gmail.com>
2022-02-16 19:00:40 +0800

    add cos/index

---
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/index.js
@@ -0,0 +1,5 @@
+exports.getSts = require('./get-sts');
+exports.getTmpCos = require('./tmp-cos');
+exports.putObject = require('./put-object');
+exports.uploadFile = require('./upload-file');
+exports.sliceUploadFile = require('./slice-upload-file');
