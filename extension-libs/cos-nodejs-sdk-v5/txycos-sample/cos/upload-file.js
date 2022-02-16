b9664983912ad180cf24f31f6c29dd54c661006b
vjudge <gradonday@gmail.com>
2022-02-16 18:58:56 +0800

    add upload-file

---
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/upload-file.js
@@ -0,0 +1,26 @@
+const config = require('config');
+const _ = require('lodash');
+
+module.exports = function uploadFile (cos, cosPath, localFilePath) {
+    const start = performance.now();
+    return new Promise((resolve, reject) => {
+        cos.uploadFile(Object.assign({}, config.get('bucket'), {
+            Key: cosPath,
+            FilePath: localFilePath,
+            // onTaskReady: function(taskId) {},
+            // onHashProgress: function (progressData) {},
+            onProgress: function (progressData) {
+                console.log(JSON.stringify(progressData));
+            }
+        }), function(err, data) {
+            const end = performance.now();
+            console.log(`资源上传时长: ${_.floor(end - start, 2)}ms`);
+            if (err) {
+                console.error('uploadFile err: ', err);
+                return reject(err);
+            }
+            console.log('uploadFile.data:', data);
+            return resolve(data);
+        });
+    });
+}
