89f409c76a3aebcdc411ba1404bb57da089ad25e
vjudge <gradonday@gmail.com>
2022-02-16 18:57:49 +0800

    add server.js

--- extension-libs/cos-nodejs-sdk-v5/txycos-sample/server.js
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/server.js
@@ -1,32 +1,27 @@
 const fs = require('fs');
-const uuid = require('uuid');
+const config = require('config');
 const COS = require('cos-nodejs-sdk-v5');
 
-const SecretId = 'AKIDEH5cssb4GBpDLCn8CzVkjQq0uXtynLcb';
-const SecretKey = 'kIabEafV4NPAevwg9LGuHNDm45qKXT8W';
-const APPID = '1253686866';
-const Bucket = 'eeo-' + APPID;
-const Region = 'ap-shanghai';
+const cns = require('./cos');
 
-const cos = new COS({ SecretId, SecretKey });
+const cosPath = 'tmp/222222.zip';
+const localFilePath = '/Users/vjudge/Downloads/apache-tomcat-9.0.52.zip';
 
-const filePath = '/Users/vjudge/Downloads/apache-tomcat-9.0.52.zip';
+main ();
 
-const time = console.time('上传花费时间: ');
-cos.putObject(Object.assign({ Bucket, Region }, {
-  Key: uuid.v4(),
-  StorageClass: 'STANDARD',
-  Body: fs.createReadStream(filePath),
-  ContentLength: fs.statSync(filePath).size,
-  // onProgress: function(progressData) {
-  //   console.log(JSON.stringify(progressData));
-  // }
-}), function(err, data) {
-  console.timeEnd('上传花费时间: ');
-    console.log(err || data);
-});
+async function main () {
+  // const cos = new COS({
+  //   SecretId: config.get('SecretId'),
+  //   SecretKey: config.get('SecretKey')
+  // });
+  const cos = cns.getTmpCos(); // 获取临时授权cos
 
-
+  // const ret = await cns.putObject(cos, cosPath, localFilePath);
+  // const ret = await cns.uploadFile(cos, cosPath, localFilePath);
+  const ret = await cns.sliceUploadFile(cos, cosPath, localFilePath);
+  console.log('====== ret', ret);
+  return ret;
+}
 
 
 
