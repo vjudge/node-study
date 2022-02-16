da362d74549945e67d95943b02359937658dd571
vjudge <gradonday@gmail.com>
2022-02-16 18:58:39 +0800

    add put object

---
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/put-object.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const config = require('config');
+const _ = require('lodash');
+
+module.exports = function putObject (cos, cosPath, localFilePath) {
+    const start = performance.now();
+    return new Promise((resolve, reject) => {
+        cos.putObject(Object.assign({}, config.get('bucket'), {
+            Key: cosPath,
+            Body: fs.createReadStream(localFilePath),
+            ContentLength: fs.statSync(localFilePath).size,
+            onProgress: function(progressData) {
+                console.log(JSON.stringify(progressData));
+            }
+        }), (err, data) => {
+            const end = performance.now();
+            console.log(`资源上传时长: ${_.floor(end - start, 2)}ms`);
+            if (err) {
+                console.error(`[${localFilePath}] Error`, err);
+                return reject(null);
+            }
+            return resolve(data);
+        });
+    });
+}
+
+
+
+
+
+
