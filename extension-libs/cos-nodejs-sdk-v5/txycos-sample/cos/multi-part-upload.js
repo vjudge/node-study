5e83a3e08cc9b2a01bb6ea4632db5ebeda8c2e1e
vjudge <gradonday@gmail.com>
2022-02-22 20:07:37 +0800

    add multiPartUpload

--- extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/multi-part-upload.js
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/multi-part-upload.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const config = require('config');
+const _ = require("lodash");
 
 exports.multipartInit = function multipartInit (cos, cosPath) {
+    const start = performance.now();
     return new Promise((resolve, reject) => {
         cos.multipartInit(Object.assign({}, config.get('bucket'), {
             Key: cosPath,
         }), (err, data) => {
+            const end = performance.now();
+            console.log(`multipartInit: ${_.floor(end - start, 2)}ms`);
             if (err) {
                 console.error('multipartInit.Error: ', err);
                 return reject(err);
@@ -16,6 +20,7 @@ exports.multipartInit = function multipartInit (cos, cosPath) {
 }
 
 exports.multipartUpload = function (cos, cosPath, UploadId, localFilePath, PartNumber = '1') {
+    const start = performance.now();
     return new Promise((resolve, reject) => {
         cos.multipartUpload(Object.assign({}, config.get('bucket'), {
             Key: cosPath,
@@ -24,6 +29,8 @@ exports.multipartUpload = function (cos, cosPath, UploadId, localFilePath, PartN
             Body: fs.createReadStream(localFilePath),
             ContentLength: fs.statSync(localFilePath).size,
         }), (err, data) => {
+            const end = performance.now();
+            console.log(`multipartUpload: ${_.floor(end - start, 2)}ms`);
             if (err) {
                 console.log('multipartUpload.Error: ', err);
                 return reject(err);
@@ -34,12 +41,15 @@ exports.multipartUpload = function (cos, cosPath, UploadId, localFilePath, PartN
 }
 
 exports.multipartComplete = function (cos, cosPath, UploadId, Parts) {
+    const start = performance.now();
     return new Promise((resolve, reject) => {
         cos.multipartComplete(Object.assign({}, config.get('bucket'), {
             Key: cosPath,
             UploadId,
             Parts,
         }), (err, data) => {
+            const end = performance.now();
+            console.log(`multipartComplete: ${_.floor(end - start, 2)}ms`);
             if (err) {
                 console.error('multipartComplete.Error: ', err);
                 return reject(err);
