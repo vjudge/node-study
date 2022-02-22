aa317d3937b50541948274327aa8457708e218b5
vjudge <gradonday@gmail.com>
2022-02-22 19:52:35 +0800

    update sample

--- extension-libs/cos-nodejs-sdk-v5/txycos-sample/server.js
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/server.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const config = require('config');
 const COS = require('cos-nodejs-sdk-v5');
+const rp = require('request-promise');
 
 const cns = require('./cos');
 
-const cosPath = 'tmp/222222.zip';
-const localFilePath = '/Users/vjudge/Downloads/apache-tomcat-9.0.52.zip';
+// const cosPath = 'tmp/222222.zip';
+// const localFilePath = '/Users/vjudge/Downloads/apache-tomcat-9.0.52.zip';
+
+const cosPath = 'tmp/222222.jpeg';
+const localFilePath = '/Users/vjudge/Downloads/architecture-diagram.jpeg';
 
 main ();
 
@@ -18,9 +22,23 @@ async function main () {
 
   // const ret = await cns.putObject(cos, cosPath, localFilePath);
   // const ret = await cns.uploadFile(cos, cosPath, localFilePath);
-  const ret = await cns.sliceUploadFile(cos, cosPath, localFilePath);
+  // const ret = await cns.sliceUploadFile(cos, cosPath, localFilePath);
+  // const ret = await uploadByUrl(cos, cosPath);
+  // const ret = await cns.getObjectUrl(cos, cosPath);
+  const ret = await cns.getDownloadUrl(cos, cosPath);
   console.log('====== ret', ret);
   return ret;
+}
+
+async function uploadByUrl (cos, cosPath) {
+  const uploadUrl = await cns.getUploadUrl(cos, cosPath);
+  const req = rp({
+    uri: uploadUrl,
+    method: 'PUT'
+  });
+  const readStream = fs.createReadStream(localFilePath);
+  readStream.pipe(req);
+  return true;
 }
 
 
