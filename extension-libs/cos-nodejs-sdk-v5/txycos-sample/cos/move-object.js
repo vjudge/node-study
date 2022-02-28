8cd7d023d5fb707bfa409f0db43f64aabeda4858
vjudge <gradonday@gmail.com>
2022-02-28 15:19:26 +0800

    add move object

--- extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/move-object.js
+++ extension-libs/cos-nodejs-sdk-v5/txycos-sample/cos/move-object.js
@@ -2,39 +2,29 @@ const _ = require('lodash');
 const config = require('config');
 
 // 移动文件(支持分片): 先复制, 再删除
-exports.moveSliceObject = function moveSliceObject (cos, cosPath, copyPath) {
+exports.moveSliceObject = async function moveSliceObject (cos, cosPath, copyPath) {
     const start = performance.now();
-    return sliceCopyFile.then((data) => {
-        cos.deleteObject(Object.assign( {
-            Key: copyPath,
-        }, config.get('bucket')), function(err, data) {
-            const end = performance.now();
-            console.log(`moveSliceObject: ${_.floor(end - start, 2)}ms`);
-            if (err) {
-                console.error('moveSliceObject Error: ', err, JSON.stringify({ cosPath, copyPath }));
-                return false;
-            }
-            return true;
-        });
-    })
+    const pcRet = await sliceCopyFile(cos, cosPath, copyPath);
+    if (!pcRet) {
+        return false;
+    }
+    const ret = await deleteObject(cos, copyPath);
+    const end = performance.now();
+    console.log(`moveSliceObject: ${_.floor(end - start, 2)}ms`);
+    return ret;
 }
 
 // 移动文件: 先复制, 再删除
-exports.moveObject = function moveObject (cos, cosPath, copyPath) {
+exports.moveObject = async function moveObject (cos, cosPath, copyPath) {
     const start = performance.now();
-    return putObjectCopy.then((data) => {
-        cos.deleteObject(Object.assign( {
-            Key: copyPath,
-        }, config.get('bucket')), function(err, data) {
-            const end = performance.now();
-            console.log(`moveObject: ${_.floor(end - start, 2)}ms`);
-            if (err) {
-                console.error('moveObject Error: ', err, JSON.stringify({ cosPath, copyPath }));
-                return false;
-            }
-            return true;
-        });
-    })
+    const pcRet = await putObjectCopy(cos, cosPath, copyPath);
+    if (!pcRet) {
+        return false;
+    }
+    const ret = await deleteObject(cos, copyPath);
+    const end = performance.now();
+    console.log(`moveObject: ${_.floor(end - start, 2)}ms`);
+    return ret;
 }
 
 // 分块复制
@@ -72,10 +62,28 @@ function putObjectCopy (cos, cosPath, copyPath) {
             const end = performance.now();
             console.log(`putObjectCopy: ${_.floor(end - start, 2)}ms`);
             if (err) {
-                console.error('putObjectCopy Error: ', err, JSON.stringify({ cosPath, copyPath }));
+                console.error('moveObject Error: ', err, JSON.stringify({ cosPath, copyPath }));
                 return reject(err);
             }
             return resolve(data);
+        });
+    });
+}
+
+// 删除对象
+function deleteObject (cos, cosPath) {
+    const start = performance.now();
+    return new Promise((resolve, reject) => {
+        cos.deleteObject(Object.assign( {
+            Key: cosPath,
+        }, config.get('bucket')), function(err, data) {
+            const end = performance.now();
+            console.log(`deleteObject: ${_.floor(end - start, 2)}ms`);
+            if (err) {
+                console.error('deleteObject Error: ', err, JSON.stringify({ cosPath, cosPath }));
+                return resolve(false);
+            }
+            return resolve(true);
         });
     });
 }
